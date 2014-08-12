d5880b15ec0fe1be473caffe506dfc7e2975b54b
Aleksey Kladov <aleksey.kladov@gmail.com>
2014-08-12 22:18:31 +0400

    refactor to use template

--- functional_tests.py
+++ functional_tests.py
@@ -44,7 +44,7 @@ class NewVisitorTest(unittest.TestCase):
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly".
-        self.fail('Finish the test')
+        self.fail('Finish the test!')
 
         # The page updates again, and now show both items on the list.
 
--- lists/tests.py
+++ lists/tests.py
@@ -14,4 +14,4 @@ class HomePageTest(TestCase):
         response = home(request)
         self.assertTrue(response.content.startswith(b'<html>'))
         self.assertIn(b'<title>To-Do lists</title>', response.content)
-        self.assertTrue(response.content.endswith(b'</html>'))
+        self.assertTrue(response.content.strip().endswith(b'</html>'))

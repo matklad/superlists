bbdb6745eb74fcba8ac672d54e99358bdb3c7fc4
Aleksey Kladov <aleksey.kladov@gmail.com>
2014-08-13 01:13:23 +0400

    separete view for lists

--- lists/tests.py
+++ lists/tests.py
@@ -18,16 +18,6 @@ class HomePageTest(TestCase):
         expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode(), expected_html)
 
-    def test_home_page_displays_all_list_items(self):
-        Item.objects.create(text='it 1')
-        Item.objects.create(text='it 2')
-
-        request = HttpRequest()
-        response = home_page(request)
-
-        self.assertIn('it 1', response.content.decode())
-        self.assertIn('it 2', response.content.decode())
-
     def test_home_page_can_save_a_POST_request(self):
         request = HttpRequest()
         request.method = 'POST'
@@ -47,12 +37,28 @@ class HomePageTest(TestCase):
         response = home_page(request)
 
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
     def test_home_page_only_saves_items_when_necessary(self):
         request = HttpRequest()
         home_page(request)
         self.assertEqual(Item.objects.count(), 0)
+
+class ListViewTestCase(TestCase):
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_displays_all_items(self):
+        Item.objects.create(text='it 1')
+        Item.objects.create(text='it 2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'it 1')
+        self.assertContains(response, 'it 2')
+
 
 class ItemModelTest(TestCase):
 

7bc93c1983bc213fe52d94baab2e796bc489e9b0
Aleksey Kladov <aleksey.kladov@gmail.com>
2014-08-13 01:37:05 +0400

    list model

--- lists/views.py
+++ lists/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
-from .models import Item
+from .models import Item, List
 
 def home_page(request):
     return render(request, 'home.html')
@@ -12,5 +12,6 @@ def view_list(request):
     return render(request, 'list.html', {'items': items})
 
 def new_list(request):
-    item = Item.objects.create(text=request.POST.get('item_text', ''))
+    list_ = List.objects.create()
+    item = Item.objects.create(text=request.POST.get('item_text', ''), list=list_)
     return redirect('/lists/the-only-list-in-the-world/')

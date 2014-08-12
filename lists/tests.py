56ca817c718727a8c820d6c3970e0c3c682677ec
Aleksey Kladov <aleksey.kladov@gmail.com>
2014-08-12 21:58:34 +0400

    home page

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,6 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
 
 from .views import home
 
@@ -7,3 +8,10 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page(self):
         found = resolve('/')
         self.assertEqual(found.func, home)
+
+    def test_home_page_return_correct_html(self):
+        request = HttpRequest()
+        response = home(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))

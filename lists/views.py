b4344ba3a887dfabbacc7be83bd075f032de1be9
Aleksey Kladov <aleksey.kladov@gmail.com>
2014-08-13 01:24:42 +0400

    separate endpoint for creating a to-do

--- lists/views.py
+++ lists/views.py
@@ -5,12 +5,12 @@ from django.shortcuts import render, redirect
 from .models import Item
 
 def home_page(request):
-    if request.method == 'POST':
-        item = Item.objects.create(text=request.POST.get('item_text', ''))
-        return redirect('/lists/the-only-list-in-the-world/')
-
     return render(request, 'home.html')
 
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+def new_list(request):
+    item = Item.objects.create(text=request.POST.get('item_text', ''))
+    return redirect('/lists/the-only-list-in-the-world/')

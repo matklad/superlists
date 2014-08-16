688f42f3f3d7d43e57db3eff3890bcb8502512e5
Aleksey Kladov <aleksey.kladov@gmail.com>
2014-08-16 22:37:31 +0400

    logout

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -19,8 +19,21 @@ class LoginTest(FunctionalTest):
 
     def wait_for(self, selector):
         WebDriverWait(self.browser, timeout=30).until(
-            lambda b: b.find_element_by_css_selector(selector)
+            lambda b: b.find_element_by_css_selector(selector),
+            'Could not find element "{}". Page text was {}'.format(
+                selector, self.find('body').text
+            )
         )
+
+    def wait_to_be_logged_in(self):
+        self.wait_for('#id_logout')
+        navbar = self.find('.navbar')
+        self.assertIn('alice@mockmyid.com', navbar.text)
+
+    def wait_to_be_logged_out(self):
+        self.wait_for('#id_login')
+        navbar = self.find('.navbar')
+        self.assertNotIn('alice', navbar.text)
 
     def test_login_with_persona(self):
         # Alice goes to the awesome superlists site and
@@ -39,6 +52,17 @@ class LoginTest(FunctionalTest):
         self.switch_to_new_window('To-Do')
 
         # Alice can see that she is logged in
-        self.wait_for('#id_logout')
-        navbar = self.find('.navbar')
-        self.assertIn('alice@mockmyid.com', navbar.text)
+        self.wait_to_be_logged_in()
+
+        # Refreshing the page, she sees it's a real session login,
+        # not just a one-off for that page
+        self.browser.refresh()
+        self.wait_to_be_logged_in()
+
+        # Terrified of this new feature, she reflexively clicks 'logout'
+        self.find('#id_logout').click()
+        self.wait_to_be_logged_out()
+
+        # The logged out status also persists after a refresh
+        self.browser.refresh()
+        self.wait_to_be_logged_out()

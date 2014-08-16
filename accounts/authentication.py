74bb3cee8faf2731d111d2940c1e669c84a6c836
Aleksey Kladov <aleksey.kladov@gmail.com>
2014-08-17 01:28:47 +0400

    more robust logging

--- accounts/authentication.py
+++ accounts/authentication.py
@@ -1,7 +1,12 @@
+import logging
+
 from django.contrib.auth import get_user_model
 from django.conf import settings
 import requests
 User = get_user_model()
+
+logger = logging.getLogger(__name__)
+
 
 PERSONA_VERIFY_URL = 'https://verifier.login.persona.org/verify'
 
@@ -15,6 +20,10 @@ class PersonaAuthenticationBackend(object):
         if response.ok and response.json()['status'] == 'okay':
             user, _ = User.objects.get_or_create(email=response.json()['email'])
             return user
+        else:
+            logger.warning(
+                'Persona says no. Json was: {}'.format(response.json())
+            )
 
     def get_user(self, email):
         try:
